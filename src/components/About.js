9f36f58a1b032bbd2530880729a38b19c043f3e7
Zachary <zp.lambda@yahoo.com>
2020-10-12 00:31:09 -0400

    Routes - UI

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,22 @@ import { logoVars } from './Animations'
 
 import logo from './assets/logo-no-bg.png';
 import logo400 from './assets/logo-no-bg-400.png';
+import profilePic from './assets/profile.jpeg';
+import pp360 from './assets/profile-360.png';
+import pp180 from './assets/profile-180.png';
 
 export const About = () => {
     const { push } = useHistory();
 
     return (
-        <>
-            {/* <motion.img className='logo' src={logo400} alt='logo' onClick={() => push('/')} variants={logoVars} initial='init' animate='after' /> */}
-        </>
+        <motion.div className='pp-container'>
+            <motion.div className='pp-upper'>
+                <motion.img className='pp' src={pp360} alt='profile picture - Zachary Peterson' />
+                <motion.div className='pp-text'>
+                    <motion.p><strong>HELLO</strong><br/>my name is:</motion.p>
+                    <motion.h2 className='pp-h2'>Zachary Peterson</motion.h2>
+                </motion.div>
+            </motion.div>
+        </motion.div>
     )
 }
